6439ada6950736af2b025feffed34c79dd9b4c04
PegasusMKD <csprogrammers@outlook.com>
2022-02-17 01:09:13 +0100

    Finished up with creating feedback module, database configuration and gateway configuration are left

---
+++ kgt-feedback/src/main/kotlin/finki/ukim/kgt/kgtfeedback/KgtFeedbackApplication.kt
@@ -0,0 +1,11 @@
+package finki.ukim.kgt.kgtfeedback
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KgtFeedbackApplication
+
+fun main(args: Array<String>) {
+    runApplication<KgtFeedbackApplication>(*args)
+}
