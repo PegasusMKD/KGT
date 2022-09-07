245ba7788279031f1a9e515f1e863ed24f72c86b
PegasusMKD <csprogrammers@outlook.com>
2022-09-07 23:03:28 +0200

    Update scripts and version of repositories

--- build_and_deploy.ps1
+++ build_and_deploy.ps1
@@ -1,2 +1,2 @@
-docker compose -f docker-compose.yml -f docker-compose.ml.yml build
-docker compose -f docker-compose.yml -f docker-compose.ml.yml push
+docker compose -f docker-compose.yml -f docker-compose.ml.yml -f docker-compose.deployment.yml build
+docker compose -f docker-compose.yml -f docker-compose.ml.yml -f docker-compose.deployment.yml push
